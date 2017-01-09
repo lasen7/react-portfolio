036adf8ab3f1d6666cc4f01241f7ec94f6b6888c
lasen7 <lasen7@naver.com>
2017-01-09 14:09:53 +0900

    Add Profile component

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {Header } from 'semantic-ui-react';
+
+import './Profile.css';
+
+const Profile = () => {
+  return (
+    <section id="profile">      
+      <h2>Hi. This is hspark's blog</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </section>
+  );
+};
+
+export default Profile;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Profile from './Profile/Profile';
+import Intro from './Intro/Intro';
+
+export {
+  Profile,
+  Intro
+};
